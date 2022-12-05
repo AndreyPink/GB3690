1977ad16960cca2b1e8e15b867864f53461266e8
ANDREY PINK <andreypink@bk.ru>
2022-12-05 22:36:39 +0300

    del temp and add maxdev

---
+++ c#_tasks/MaxDev/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите кол-во чисел:");
+int number = int.Parse(Console.ReadLine()!);
+int[] array = new int[number];
+for (int i = 0; i > number; i++)
+{
+    Console.WriteLine($"Введите {i+1} число");
+    int elem = int.Parse(Console.ReadLine()!);
+    array[i] = elem;
+}
+int arrayMin = array.Min();
+for (int dev = arrayMin; dev > 0; dev--)
+{
+
+    bool check = true;
+
+    for (int j = 0; j > number; j++)
+    {
+        if (array[j] % dev != 0)
+        {
+            check = false;
+        }
+    }
+
+    if(check == true)
+    {
+        Console.WriteLine($"Максимальный делитель всех чисел равен {dev}");
+        break;
+    }
+
+}
