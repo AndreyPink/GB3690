bd4783c3e950ec51f58f682199bfcd1607e542af
ANDREY PINK <andreypink@bk.ru>
2022-12-05 21:20:25 +0300

    add mayBePalindrome

---
+++ c#_tasks/mayBePalindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите слово:");
+string text = Console.ReadLine()!;
+string newText = "";
+int len = text.Length;
+for (int i = 0; i < len; i++)
+{
+    bool check = newText.Contains(text[i]);
+    if (!check)
+    {
+        newText += text[i];
+    }
+    else
+    {
+        string elem = (text[i]).ToString();
+        newText = newText.Replace(elem, "");
+        // newText = Regex.Replace(newText,elem , "");
+    }
+}
+if (newText.Length < 2)
+{
+    Console.WriteLine("Из этого слова можно составить палиндром");
+}
+else
+{
+    Console.WriteLine("Из этого слова нельзя составить палиндром");
+}
+Console.WriteLine(newText.Length);
