e1541b7bd38eb8aba9f974a88cf87e1ec6d060e3
ANDREY PINK <andreypink@bk.ru>
2022-12-09 00:58:13 +0300

    add Hanoi

--- c#_tasks/HanoiTower_lite/Program.cs
+++ c#_tasks/HanoiTower_lite/Program.cs
@@ -6,7 +6,7 @@ void Step(int n, string a, string b, string c)
     Step(n - 1, a, c, b);
     countStep ++;
     Console.WriteLine($"Ход {countStep}");
-    Console.WriteLine($"Переложить диск с {a} стержня на {b} стержень");
+    Console.WriteLine($"Перенеси диск с {a} стержня на {b} стержень");
     Console.WriteLine("Жми ENTER");
     Console.ReadKey();
     Step(n - 1, c, b, a);
@@ -18,4 +18,4 @@ int num = int.Parse(Console.ReadLine()!);
 Console.WriteLine($"Ханойская башня состоит из {num} дисков");
 Console.WriteLine();
 Step(num, "I", "II", "III");
-Console.WriteLine($"Все диски переложены за {countStep} шагов");
+Console.WriteLine($"Все диски перенесены за {countStep} шагов");
