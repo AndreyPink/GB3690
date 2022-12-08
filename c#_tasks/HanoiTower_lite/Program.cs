1ede88898d9fd28ae47a1a521cff0028f846ccab
ANDREY PINK <andreypink@bk.ru>
2022-12-09 00:56:01 +0300

    add Hanoi

---
+++ c#_tasks/HanoiTower_lite/Program.cs
@@ -0,0 +1,21 @@
+﻿int countStep = 0; 
+void Step(int n, string a, string b, string c)
+{
+    if (n > 0) 
+    {
+    Step(n - 1, a, c, b);
+    countStep ++;
+    Console.WriteLine($"Ход {countStep}");
+    Console.WriteLine($"Переложить диск с {a} стержня на {b} стержень");
+    Console.WriteLine("Жми ENTER");
+    Console.ReadKey();
+    Step(n - 1, c, b, a);
+    }
+}
+Console.Clear();
+Console.WriteLine("Введите кол-во дисков:");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Ханойская башня состоит из {num} дисков");
+Console.WriteLine();
+Step(num, "I", "II", "III");
+Console.WriteLine($"Все диски переложены за {countStep} шагов");
---
+++ c#_tasks/HanoiTowers/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// string s1_1 = "          _______            ";
+// string s1_2 = "          |XXXXX|            ";
+
+// string s2_1 = "        ____________         ";
+// string s2_2 = "        |XXXXXXXXXX|         ";
+
+// string s3_1 = "     __________________      ";
+// string s3_2 = "     |XXXXXXXXXXXXXXXX|      ";
+
+// string s4_1 = "   ______________________    ";
+// string s4_2 = "   |XXXXXXXXXXXXXXXXXXXX|    ";
+
+// string s5_1 = "_____________________________";
+// string s5_2 = "|XXXXXXXXXXXXXXXXXXXXXXXXXXX|";
+
+// string s_em = "                             ";
+
+// string[] arr1_1 = {s1_1, s_em, s_em}; 
+// string[] arr1_2 = {s1_2, s_em, s_em}; 
+// string[] arr2_1 = {s2_1, s_em, s_em}; 
+// string[] arr2_2 = {s2_2, s_em, s_em}; 
+// string[] arr3_1 = {s3_1, s_em, s_em}; 
+// string[] arr3_2 = {s3_2, s_em, s_em}; 
+// string[] arr4_1 = {s4_1, s_em, s_em}; 
+// string[] arr4_2 = {s4_2, s_em, s_em}; 
+// string[] arr5_1 = {s5_1, s_em, s_em}; 
+// string[] arr5_2 = {s5_2, s_em, s_em}; 
+
+
+// void Paint()
+// {
+// Console.Clear();
+// Console.WriteLine($"{arr1_1[0]}{arr1_1[1]}{arr1_1[2]}");
+// Console.WriteLine($"{arr1_2[0]}{arr1_2[1]}{arr1_2[2]}");
+// Console.WriteLine($"{arr2_1[0]}{arr2_1[1]}{arr2_1[2]}");
+// Console.WriteLine($"{arr2_2[0]}{arr2_2[1]}{arr2_2[2]}");
+// Console.WriteLine($"{arr3_1[0]}{arr3_1[1]}{arr3_1[2]}");
+// Console.WriteLine($"{arr3_2[0]}{arr3_2[1]}{arr3_2[2]}");
+// Console.WriteLine($"{arr4_1[0]}{arr4_1[1]}{arr4_1[2]}");
+// Console.WriteLine($"{arr4_2[0]}{arr4_2[1]}{arr4_2[2]}");
+// Console.WriteLine($"{arr5_1[0]}{arr5_1[1]}{arr5_1[2]}");
+// Console.WriteLine($"{arr5_2[0]}{arr5_2[1]}{arr5_2[2]}");
+// Console.WriteLine("______________|____________________________|____________________________|______________");
+// Console.WriteLine("                                                                                       ");
+// }
+
